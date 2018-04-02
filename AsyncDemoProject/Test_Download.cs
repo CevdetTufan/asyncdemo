ee60695887e09d06320d63b95f6f4077805728ba
Cevdet Tufan <tufan_022@hotmail.com>
2018-04-02 13:01:12 +0300

    Download

---
+++ AsyncDemoProject/Test_Download.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncDemoProject
+{
+    [TestClass]
+    public class Test_Download
+    {
+        string url = "http://www.deelay.me/5000/www.delsink.com";
+
+        [TestMethod]
+        public void TestDownload()
+        {
+            var httpRequestInfo = HttpWebRequest.Create(url);
+            var callBack = new AsyncCallback(HttpResponseAvailable);
+            var asyncResult = httpRequestInfo.BeginGetResponse(callBack, httpRequestInfo);
+
+            asyncResult.AsyncWaitHandle.WaitOne();
+        }
+
+        private static void HttpResponseAvailable(IAsyncResult asyncResult)
+        {
+            var httpRequestInfo = asyncResult.AsyncState as HttpWebRequest;
+            var httpResponseInfo = httpRequestInfo.EndGetResponse(asyncResult) as HttpWebResponse;
+
+            var responseStream = httpResponseInfo.GetResponseStream();
+            using (StreamReader sr = new StreamReader(responseStream))
+            {
+                var webPage = sr.ReadToEnd();
+            }
+        }
+    }
+}
